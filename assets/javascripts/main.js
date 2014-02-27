578ff6961ec5ea1fad67a0ac9837d5c06c342b1d
Rich <toneworm@gmail.com>
2014-02-27 00:52:16 +0000

    Background colour test...

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -2,26 +2,17 @@
 
 	var dataset = [ 5, 10, 15, 20 ];
 
-	// d3.select("body")
-	//    	.selectAll("p")
-	//    	.data(dataset)
-	//    	.enter()
-	//    	.append("p")
-	//    	.text("New paragraph");
-
-	// d3.select("body").selectAll("p")
- //    .data(dataset)
- //    .enter()
- //    .append("p")
- //    .text("New paragraph!");
-
- 	// alert("test");
-
  	d3.select(".container")
  		.selectAll("p")
 	    .data(dataset)
 	    .enter()
 	    .append("p")
+	    .style("background-color", function (d) {
+	    	var color = Math.round(d/20 * 255),
+	    		colorStr = [color, color, color].join();
+
+	    	return "rgb(" + colorStr + ")";
+	    })
 	    .text(function(d) { return d; });
 
 }(jQuery));
